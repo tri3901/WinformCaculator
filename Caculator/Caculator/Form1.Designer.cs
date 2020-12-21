e1a8e34a6f7e2b0d1355680ddfd14bf7ff9e5bc5
Hồ Trí <hodangtri123@gmail.com>
2020-12-21 20:58:08 +0700

    Caculator using Winform C#

---
+++ Caculator/Caculator/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Caculator
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.lblOperand1 = new System.Windows.Forms.Label();
+            this.lblOperand2 = new System.Windows.Forms.Label();
+            this.txtOperand1 = new System.Windows.Forms.TextBox();
+            this.txtOperand2 = new System.Windows.Forms.TextBox();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(44, 94);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(47, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(97, 94);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(47, 23);
+            this.btnSubtract.TabIndex = 5;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(150, 94);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(47, 23);
+            this.btnMultiply.TabIndex = 6;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(203, 94);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(47, 23);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // lblOperand1
+            // 
+            this.lblOperand1.AutoSize = true;
+            this.lblOperand1.Location = new System.Drawing.Point(45, 29);
+            this.lblOperand1.Name = "lblOperand1";
+            this.lblOperand1.Size = new System.Drawing.Size(80, 17);
+            this.lblOperand1.TabIndex = 0;
+            this.lblOperand1.Text = "Operand 1:";
+            // 
+            // lblOperand2
+            // 
+            this.lblOperand2.AutoSize = true;
+            this.lblOperand2.Location = new System.Drawing.Point(45, 63);
+            this.lblOperand2.Name = "lblOperand2";
+            this.lblOperand2.Size = new System.Drawing.Size(80, 17);
+            this.lblOperand2.TabIndex = 2;
+            this.lblOperand2.Text = "Operand 2:";
+            // 
+            // txtOperand1
+            // 
+            this.txtOperand1.Location = new System.Drawing.Point(131, 29);
+            this.txtOperand1.Name = "txtOperand1";
+            this.txtOperand1.Size = new System.Drawing.Size(119, 22);
+            this.txtOperand1.TabIndex = 1;
+            this.txtOperand1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtOperand2
+            // 
+            this.txtOperand2.Location = new System.Drawing.Point(131, 63);
+            this.txtOperand2.Name = "txtOperand2";
+            this.txtOperand2.Size = new System.Drawing.Size(119, 22);
+            this.txtOperand2.TabIndex = 3;
+            this.txtOperand2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(45, 134);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(58, 17);
+            this.lblAnswer.TabIndex = 8;
+            this.lblAnswer.Text = "Answer:";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(109, 131);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(141, 22);
+            this.txtAnswer.TabIndex = 9;
+            this.txtAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(44, 170);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(100, 23);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(150, 170);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(100, 23);
+            this.btnExit.TabIndex = 11;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(295, 223);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.txtOperand2);
+            this.Controls.Add(this.txtOperand1);
+            this.Controls.Add(this.lblOperand2);
+            this.Controls.Add(this.lblOperand1);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Caculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Label lblOperand1;
+        private System.Windows.Forms.Label lblOperand2;
+        private System.Windows.Forms.TextBox txtOperand1;
+        private System.Windows.Forms.TextBox txtOperand2;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Caculator/Caculator/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Caculator
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            float operand1 = float.Parse(txtOperand1.Text);
+            float operand2 = float.Parse(txtOperand2.Text);
+            float sum = operand1 + operand2;
+            txtAnswer.Text = sum.ToString();
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            float operand1 = float.Parse(txtOperand1.Text);
+            float operand2 = float.Parse(txtOperand2.Text);
+            float sum = operand1 - operand2;
+            txtAnswer.Text = sum.ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            float operand1 = float.Parse(txtOperand1.Text);
+            float operand2 = float.Parse(txtOperand2.Text);
+            float sum = operand1 * operand2;
+            txtAnswer.Text = sum.ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            float operand1 = float.Parse(txtOperand1.Text);
+            float operand2 = float.Parse(txtOperand2.Text);
+            float sum = operand1 / operand2;
+            txtAnswer.Text = sum.ToString();
+        }
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            foreach (TextBox textBox in Controls.OfType<TextBox>())
+                textBox.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
